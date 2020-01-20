71e0bbcfdc7ef120528d795c75baffc445d62758
Wanezzi <Wanezzi@live.com>
2020-01-20 15:50:50 +0100

    Update Navbar for everypage and links to php
    Update to navbar and all pages should now have navbar etc. 
    All links in the navbar have been updated to point to the right php files

--- Login.php
+++ Login.php
@@ -1,10 +1,14 @@
+<?php
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
 	<title>Login </title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="shortcut icon" type="image/png" href="assets/images/favicon.png"/>
+    <link rel="shortcut icon" type="image/png" href="assets/images/pepega.png"/>
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
           integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
@@ -17,6 +21,51 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"/>
 </head>
 <body>
+    
+    <!-- Navigation
+    ================================================== -->
+<div class="hero-background">
+    <div>
+        <img class="strips" src="assets/images/strips.png">
+    </div>
+    <div class="container">
+        <div class="header-container header">
+            <a href="Login.html">
+                <button class="header-btn"> Log in </button>
+            </a>
+            <div class="header-right">
+                <a class="navbar-item" href="#team"></a>
+                <a class="navbar-item" href="StudRegister.php">Registrering Student</a>
+                <a class="navbar-item" href="ForeleserRegister.php">Registrering Foreleser</a>
+            </div>
+        </div>
+        <!--navigation-->
+
+
+        <!-- Hero-Section
+          ================================================== -->
+
+        <div class="hero row">
+            <div class="hero-right col-sm-6 col-sm-6">
+                <h1 class="header-headline bold"> Student? <br></h1>
+                <h4 class="header-running-text light"> Alle studenter kan registrere seg her slik at dere kan få logget inn og gitt deres tilbakemeldinger!  </h4>
+                <a href="StudRegister.html">
+                    <button class="hero-btn"> Registrer Her </button>
+                </a>
+            </div><!--hero-left-->
+
+            <div class="col-sm-6 col-sm-6 ipad">
+                <img class="ipad-screen img-responsive" src="assets/images/supremeleader.jpg"/>
+            </div>
+
+            <div><img class="mouse" src="assets/images/mouse.svg"/></div>
+
+        </div><!--hero-->
+
+    </div> <!--hero-container-->
+
+</div><!--hero-background-->
+    
         <div class="limiter">
                 <div class="container-login100">
                     <div class="wrap-login100">
