a0b88bea3ca54f65a759d2ac8a550edcb14fc6a6
Wanezzi <Wanezzi@live.com>
2020-01-20 17:11:08 +0100

    DB adjustment 2.0
    REEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE

--- Sqlconn.php
+++ Sqlconn.php
@@ -1,7 +1,7 @@
 <?php
 	include('dbconfig.php');
 
-	$db_connect = new mysqli($host, $username, $passord, $dbname);
+	$db_connect = new mysqli($host, $username, $password, $dbname);
 
     //Print error message if no connection
 	if($db_connect->connect_error) {
